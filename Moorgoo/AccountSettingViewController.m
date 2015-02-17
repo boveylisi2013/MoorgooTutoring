1cecce86c6ee332d29a62b6b724ab15f75820923
Shawn Li <shawnli2010@126.com>
2015-02-17 13:16:12 -0800

    fix pay button bug, fix label too long bug, still need to fix table view of payment history bug

--- Moorgoo/AccountSettingViewController.m
+++ Moorgoo/AccountSettingViewController.m
@@ -11,15 +11,15 @@
 @interface AccountSettingViewController () <UITextFieldDelegate,UIPickerViewDelegate,UIPickerViewDataSource>
 {
     UIPickerView *schoolPicker;
-    UIPickerView *departmentPicker;
+    //UIPickerView *departmentPicker;
     NSMutableArray *pickerSchoolArray;
-    NSMutableArray *pickerDepartmentArray;
+    //NSMutableArray *pickerDepartmentArray;
     
-    NSString *currentUserFirstName;
-    NSString *currentUserLastName;
+    //NSString *currentUserFirstName;
+    //NSString *currentUserLastName;
     NSString *currentUserPhoneNumber;
     NSString *currentUserSchool;
-    NSString *currentUserDepartment;
+    //NSString *currentUserDepartment;
 }
 @end
 
@@ -34,31 +34,31 @@
     PFUser *currentUser = [PFUser currentUser];
     
     // Query data for late passing to other view controller
-    currentUserFirstName = [currentUser objectForKey:@"firstName"];
-    currentUserLastName = [currentUser objectForKey:@"lastName"];
+    //currentUserFirstName = [currentUser objectForKey:@"firstName"];
+    //currentUserLastName = [currentUser objectForKey:@"lastName"];
     currentUserPhoneNumber = [currentUser objectForKey:@"phone"];
     currentUserSchool = [currentUser objectForKey:@"school"];
-    currentUserDepartment = [currentUser objectForKey:@"department"];
+    //currentUserDepartment = [currentUser objectForKey:@"department"];
     
     // Prefill the textFields
-    self.firstNameTextField.text = currentUserFirstName;
-    self.lastNameTextField.text = currentUserLastName;
+    //self.firstNameTextField.text = currentUserFirstName;
+    //self.lastNameTextField.text = currentUserLastName;
     self.phoneNumberTextField.text = currentUserPhoneNumber;
     self.schoolTextField.text = currentUserSchool;
-    self.departmentTextField.text = currentUserDepartment;
+    //self.departmentTextField.text = currentUserDepartment;
     
     //keyboard disappear when tapping outside of text field
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
     [self.view addGestureRecognizer:tap];
     
     [self addSchoolPicker];
-    [self addDepartmentPicker];
+    //[self addDepartmentPicker];
     
-    self.firstNameTextField.delegate = self;
-    self.lastNameTextField.delegate = self;
+    //self.firstNameTextField.delegate = self;
+    //self.lastNameTextField.delegate = self;
     self.phoneNumberTextField.delegate = self;
     self.schoolTextField.delegate = self;
-    self.departmentTextField.delegate = self;
+    //self.departmentTextField.delegate = self;
     
     //self.phoneNumberTextField.keyboardType =  UIKeyboardTypeDecimalPad;
 }
@@ -121,53 +121,53 @@
 }
 
 //UIpicker view replace keyborad for department
--(void)addDepartmentPicker{
-    pickerDepartmentArray = [[NSMutableArray alloc]init];
-    [self getDepartments];
-    
-    departmentPicker = [[UIPickerView alloc] initWithFrame:CGRectZero];
-    departmentPicker.delegate = self;
-    departmentPicker.dataSource = self;
-    [departmentPicker setShowsSelectionIndicator:YES];
-    departmentTextField.inputView = departmentPicker;
-    
-    // Create done button in UIPickerView
-    UIToolbar*  mypickerToolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 320, 20)];
-    mypickerToolbar.barStyle = UIBarStyleBlackOpaque;
-    [mypickerToolbar sizeToFit];
-    NSMutableArray *barItems = [[NSMutableArray alloc] init];
-    UIBarButtonItem *flexSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
-    [barItems addObject:flexSpace];
-    
-    UIBarButtonItem *doneBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(pickerDoneClicked2)];
-    [barItems addObject:doneBtn];
-    
-    [mypickerToolbar setItems:barItems animated:YES];
-    departmentTextField.inputAccessoryView = mypickerToolbar;
-}
-
--(void)pickerDoneClicked2
-{
-    //NSLog(@"Done Clicked");
-    [departmentTextField resignFirstResponder];
-}
-
--(void)getDepartments {
-    
-    PFQuery *query = [PFQuery queryWithClassName:@"department"];
-    [query setLimit:1000];
-    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (!error) {
-            [pickerDepartmentArray addObjectsFromArray:[objects valueForKey:@"department"]];
-            [pickerDepartmentArray insertObject:@"" atIndex:0];
-            [pickerDepartmentArray sortUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
-            //NSLog(@"Successfully retrieved: %@", pickerDepartmentArray);
-        } else {
-            NSString *errorString = [[error userInfo] objectForKey:@"error"];
-            NSLog(@"Error: %@", errorString);
-        }
-    }];
-}
+//-(void)addDepartmentPicker{
+//    pickerDepartmentArray = [[NSMutableArray alloc]init];
+//    [self getDepartments];
+//    
+//    departmentPicker = [[UIPickerView alloc] initWithFrame:CGRectZero];
+//    departmentPicker.delegate = self;
+//    departmentPicker.dataSource = self;
+//    [departmentPicker setShowsSelectionIndicator:YES];
+//    departmentTextField.inputView = departmentPicker;
+//    
+//    // Create done button in UIPickerView
+//    UIToolbar*  mypickerToolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 320, 20)];
+//    mypickerToolbar.barStyle = UIBarStyleBlackOpaque;
+//    [mypickerToolbar sizeToFit];
+//    NSMutableArray *barItems = [[NSMutableArray alloc] init];
+//    UIBarButtonItem *flexSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
+//    [barItems addObject:flexSpace];
+//    
+//    UIBarButtonItem *doneBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(pickerDoneClicked2)];
+//    [barItems addObject:doneBtn];
+//    
+//    [mypickerToolbar setItems:barItems animated:YES];
+//    departmentTextField.inputAccessoryView = mypickerToolbar;
+//}
+//
+//-(void)pickerDoneClicked2
+//{
+//    //NSLog(@"Done Clicked");
+//    [departmentTextField resignFirstResponder];
+//}
+//
+//-(void)getDepartments {
+//    
+//    PFQuery *query = [PFQuery queryWithClassName:@"department"];
+//    [query setLimit:1000];
+//    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+//        if (!error) {
+//            [pickerDepartmentArray addObjectsFromArray:[objects valueForKey:@"department"]];
+//            [pickerDepartmentArray insertObject:@"" atIndex:0];
+//            [pickerDepartmentArray sortUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+//            //NSLog(@"Successfully retrieved: %@", pickerDepartmentArray);
+//        } else {
+//            NSString *errorString = [[error userInfo] objectForKey:@"error"];
+//            NSLog(@"Error: %@", errorString);
+//        }
+//    }];
+//}
 
 #pragma mark - Picker View Data source
 - (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
@@ -175,10 +175,10 @@
 }
 
 - (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent: (NSInteger)component {
-    if(pickerView == schoolPicker)
+//    if(pickerView == schoolPicker)
         return pickerSchoolArray.count;
-    else
-        return pickerDepartmentArray.count;
+//    else
+//        return pickerDepartmentArray.count;
 }
 
 #pragma mark- Picker View Delegate
@@ -186,21 +186,21 @@
             titleForRow:(NSInteger)row
            forComponent:(NSInteger)component
 {
-    if(pickerView == schoolPicker)
+//    if(pickerView == schoolPicker)
         return [pickerSchoolArray objectAtIndex:row];
-    else
-        return [pickerDepartmentArray objectAtIndex:row];
+//    else
+//        return [pickerDepartmentArray objectAtIndex:row];
 }
 
 - (void)pickerView:(UIPickerView *)pickerView
       didSelectRow:(NSInteger)row
        inComponent:(NSInteger)component
 {
-    NSLog(@"Selected Row %ld", (long)row);
-    if(pickerView == schoolPicker)
+//    NSLog(@"Selected Row %ld", (long)row);
+//    if(pickerView == schoolPicker)
         self.schoolTextField.text = [NSString stringWithFormat:@"%@", [pickerSchoolArray objectAtIndex:row]];
-    else
-        self.departmentTextField.text = [NSString stringWithFormat:@"%@", [pickerDepartmentArray objectAtIndex:row]];
+//    else
+//        self.departmentTextField.text = [NSString stringWithFormat:@"%@", [pickerDepartmentArray objectAtIndex:row]];
 }
 
 #pragma mark- buttonPressed methods
@@ -209,12 +209,12 @@
     // The string contains all possible errors
     NSString *errorString = @"";
     
-    if (self.firstNameTextField.text.length == 0) {
-        errorString = [errorString stringByAppendingString:@"Please input your first name\n"];
-    }
-    if (self.lastNameTextField.text.length == 0) {
-        errorString = [errorString stringByAppendingString:@"Please input your last name\n"];
-    }
+//    if (self.firstNameTextField.text.length == 0) {
+//        errorString = [errorString stringByAppendingString:@"Please input your first name\n"];
+//    }
+//    if (self.lastNameTextField.text.length == 0) {
+//        errorString = [errorString stringByAppendingString:@"Please input your last name\n"];
+//    }
     if (self.phoneNumberTextField.text.length == 0) {
         errorString = [errorString stringByAppendingString:@"Please input your phone number\n"];
     }
@@ -224,11 +224,11 @@
 
     
     PFUser *user = [PFUser currentUser];
-    [user setObject:self.firstNameTextField.text forKey:@"firstName"];
-    [user setObject:self.lastNameTextField.text forKey:@"lastName"];
+//    [user setObject:self.firstNameTextField.text forKey:@"firstName"];
+//    [user setObject:self.lastNameTextField.text forKey:@"lastName"];
     [user setObject:self.phoneNumberTextField.text forKey:@"phone"];
     [user setObject:self.schoolTextField.text forKey:@"school"];
-    [user setObject:self.departmentTextField.text forKey:@"department"];
+//    [user setObject:self.departmentTextField.text forKey:@"department"];
     user.password = self.passwordTextField.text;
     
     // Check whether the user inputs their information correctly or not
